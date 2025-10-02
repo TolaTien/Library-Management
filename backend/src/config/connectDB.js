c88af380795c2a82002407d1a0e3db0451c01d4b
TolaTien <tien0bip009@gmail.com>
2025-10-02 10:15:31 +0700

    connect

---
+++ backend/src/config/connectDB.js
@@ -0,0 +1,22 @@
+const {Sequelize} = require('sequelize');
+
+const connect = new Sequelize(
+    'mydb',
+    'root',
+    '123456789',
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: '3306',
+    }
+);
+
+const connectDB = async () =>{
+    try{
+        await connect.authenticate();
+        console.log("✅ Connect Database Success!");
+    }catch(err){
+        console.error('❌ Error connect database: ', err);
+    }
+};
+module.exports = {connectDB, connect};

76f85084b1b4a7e6368ab694f52562df21f9cede
TolaTien <tien0bip009@gmail.com>
2025-10-24 01:57:05 +0700

    'token'

---
+++ backend/src/services/tokenServices.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const createToken = (payload) => {
+    return jwt.sign(payload, JWT_SECRET, {expiresIn: '15m' })
+};
+
+const createRefreshToken = (payload) => {
+    return jwt.sign(payload, JWT_SECRET, {expiresIn: '7d' })
+};
+
+const verifyToken = (token) => {
+    try{
+        return jwt.verify(token, JWT_SECRET);
+    }catch(err){
+        throw new err("Vui lòng đăng nhập lại ")
+    }
+};
+
+module.exports = {
+    createToken,
+    createRefreshToken,
+    verifyToken
+};

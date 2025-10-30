f9aab497ec0dcae3f1e6649499da265d43aededa
gbach1703 <dbach17032006@gmail.com>
2025-10-30 17:10:04 +0700

    product model

--- backend/src/config/connectDB.js
+++ backend/src/config/connectDB.js
@@ -1,22 +1,17 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const connect = new Sequelize(
-    'mydb',
-    'root',
-    '123456789',
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: '3306',
-    }
-);
+const connect = new Sequelize("mydb", "root", "17032006", {
+  host: "localhost",
+  dialect: "mysql",
+  port: "3306",
+});
 
-const connectDB = async () =>{
-    try{
-        await connect.authenticate();
-        console.log("✅ Connect Database Success!");
-    }catch(err){
-        console.error('❌ Error connect database: ', err);
-    }
+const connectDB = async () => {
+  try {
+    await connect.authenticate();
+    console.log("✅ Connect Database Success!");
+  } catch (err) {
+    console.error("❌ Error connect database: ", err);
+  }
 };
-module.exports = {connectDB, connect};
+module.exports = { connectDB, connect };

2f55ac94462a3ef7f9bcf52cfc260cf614f6d535
TolaTien <tien0bip009@gmail.com>
2025-11-08 21:12:29 +0700

    fix admin

--- backend/src/routes/historyBook.routes.js
+++ backend/src/routes/historyBook.routes.js
@@ -5,9 +5,9 @@ const { authUser, check } = require('../auth/checkAuth');
 const ControllerHistoryBook = require('../controllers/historyBook.controller')
 
 router.post('/create', authUser, check(ControllerHistoryBook.createHistoryBook));
-router.post('/post', authUser, check(ControllerHistoryBook.cancelBook));
 router.get('/get-history-user', authUser, check(ControllerHistoryBook.getHistoryUser));
+router.post('/cancel-book', authUser, check(ControllerHistoryBook.cancelBook));
 router.get('/get-all-history-book', check(ControllerHistoryBook.getAllHistoryBook));
-router.post('/update-status-book',check(ControllerHistoryBook.updateStatusBook));
+router.post('/update-status-book', check(ControllerHistoryBook.updateStatusBook));
 
 module.exports = router
--- frontend/src/pages/DashbroadComponents/BookManagement.jsx
+++ frontend/src/pages/DashbroadComponents/BookManagement.jsx
@@ -186,7 +186,7 @@ const BookManagement = () => {
             const urlImage = await requestUploadImageProduct(formData);
             const data = {
                 ...values,
-                image: urlImage.metadata,
+                image: urlImage.data,
             };
 
             await requestCreateProduct(data);
@@ -231,7 +231,7 @@ const BookManagement = () => {
                     const formData = new FormData();
                     formData.append('image', newFile.originFileObj);
                     const urlImage = await requestUploadImageProduct(formData);
-                    imageUrl = urlImage.metadata;
+                    imageUrl = urlImage.data;
                 }
             }
 

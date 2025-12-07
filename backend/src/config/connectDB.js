483e4fd537f0f900f534c44e1dc30427b4721b6c
Do Quoc Huy <dqhuy132006@gmail.com>
2025-12-07 21:06:11 +0700

    mới

--- backend/src/config/connectDB.js
+++ backend/src/config/connectDB.js
@@ -2,9 +2,9 @@ const { Sequelize } = require("sequelize");
 
 
 const connect = new Sequelize(
-    'library',
+    'test',
     'root',
-    '123456789',
+    'dqhuy132006',
     {
         host: 'localhost',
         dialect: 'mysql',
